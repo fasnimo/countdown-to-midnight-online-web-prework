51dfa7838467c368fc8e4b2062a4130ebb878f22
Rene Cocom <fasnimo@gmail.com>
2019-03-26 04:33:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -9,12 +9,12 @@ end
 "HAPPY NEW YEAR!"
 end
 
- def countdown_with_sleep(number)
-  number = 10 
-  until number == 0
-  puts "#{number} SECOND(S)!"
-  break if number == 0
-  number -= 1
+def countdown_with_sleep(yeld)
+  runtime = 5
+  yeld = 5
+  until yeld == 0
+  puts "#{yeld} SECOND(S)!"
   sleep(5)
- end
- end
+  yeld -= 1
+end
+end
