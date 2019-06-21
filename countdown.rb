430fc8ecd6cc8e41dfd2e5ac5719b16726f1ca66
Rene Cocom <fasnimo@gmail.com>
2019-06-21 12:30:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,8 +6,8 @@ def countdown(number)
   # break if number == 0
   number -= 1
 end
-# "HAPPY NEW YEAR!"
-100
+ "HAPPY NEW YEAR!"
+# 100
 end
 puts countdown(15)
 
@@ -15,7 +15,7 @@ def countdown_with_sleep(yeld)
    yeld = 5
   until yeld == 0
   puts "#{yeld} SECOND(S)!"
-  sleep(5)
+  sleep(1)
   break if yeld == 0
   yeld -= 1
 end
